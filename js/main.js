11c13cf7cc64047279db99a22279819777e451d0
Mayonnaise-Sann <863193611@qq.com>
2022-08-22 22:53:30 +0800

    添加门外三个场景

--- js/main.js
+++ js/main.js
@@ -10,26 +10,56 @@ var panorama_hall = null;
 var panorama_court = null;
 var panorama_roomB = null;
 var panorama_roomA = null;
+var panorama_foreyard = null;
+var panorama_leftAlley = null;
+var panorama_rightAlley = null;
+
+function loadCommon() {
+    panorama_foreyard = new PANOLENS.ImagePanorama('./images/foreyard.jpg');
+    panorama_leftAlley  = new PANOLENS.ImagePanorama('./images/leftAlley.jpg');    
+    panorama_rightAlley = new PANOLENS.ImagePanorama('./images/rightAlley.jpg');
+    
+    panorama_foreyard.link(panorama_leftAlley, new THREE.Vector3(-5000.00, -802.45, -21.61));
+    panorama_foreyard.link(panorama_rightAlley, new THREE.Vector3(5000.00, -846.77, -20.53));
+    
+    panorama_leftAlley.link(panorama_foreyard, new THREE.Vector3(-1498.51, -1550.07, 5000.00));
+    panorama_leftAlley.link(panorama_rightAlley, new THREE.Vector3(-1150.65, 348.09, 5000.00));
+    
+    panorama_rightAlley.link(panorama_foreyard, new THREE.Vector3(5000.00, -816.07, 211.07));
+    panorama_rightAlley.link(panorama_leftAlley, new THREE.Vector3(5000.00, -157.36, -94.67));
+}
 
 function loadDay() {
-        
+    loadCommon();
     panorama_hall = new PANOLENS.ImagePanorama('./images/day/hall.jpg');
     panorama_courtyard = new PANOLENS.ImagePanorama('./images/day/courtyard.jpg');
     panorama_roomB = new PANOLENS.ImagePanorama('./images/day/roomB.jpg');
     panorama_roomA = new PANOLENS.ImagePanorama('./images/day/roomA.jpg');
     
+
     panorama_courtyard.link(panorama_hall, new THREE.Vector3(5000.00, -1594.97, 47.48));
     panorama_courtyard.link(panorama_roomA, new THREE.Vector3(-402.12, -2050.45, -5000.00));
     panorama_courtyard.link(panorama_roomB, new THREE.Vector3(5000.00, -106.88, -1394.50));
+    panorama_courtyard.link(panorama_foreyard, new THREE.Vector3(-5000.00, -1427.79, 102.78));
+
+    
     
     panorama_hall.link(panorama_courtyard, new THREE.Vector3(5000.00, -2171.89, 209.66));
     panorama_hall.link(panorama_roomB, new THREE.Vector3(2353.49, -630.82, 5000.00));
     
     panorama_roomA.link(panorama_courtyard, new THREE.Vector3(346.63, -512.45, -5000.00));
     panorama_roomB.link(panorama_hall, new THREE.Vector3(-3577.89, -1600.52, 5000.00));
+
+    panorama_foreyard.link(panorama_courtyard, new THREE.Vector3(393.70, -2186.97, -5000.00));
+
     
 }
+
+
+
+
 function loadNight() {
+    // loadCommon();
         
     panorama_hall = new PANOLENS.ImagePanorama('./images/night/hall.jpg');
     panorama_courtyard = new PANOLENS.ImagePanorama('./images/night/courtyard.jpg');
@@ -45,16 +75,20 @@ function loadNight() {
     
     panorama_roomA.link(panorama_courtyard, new THREE.Vector3(346.63, -512.45, -5000.00));
     panorama_roomB.link(panorama_hall, new THREE.Vector3(5000.00, -2396.31, -328.22));
+
+    // panorama_courtyard.link(panorama_foreyard, new THREE.Vector3(-792.87, -1297.96, -5000.00));
+    // panorama_foreyard.link(panorama_courtyard, new THREE.Vector3(393.70, -2186.97, -5000.00));
     
 }
 // --------------------------------------------
 
 const toggle = document.querySelector('#toggle');
+
 toggle.addEventListener('change', () => {
     if (toggle.value == 'day') {
         viewer.dispose();
         loadDay();
-        viewer.add(panorama_courtyard, panorama_hall, panorama_roomB, panorama_roomA);
+        viewer.add(panorama_foreyard, panorama_leftAlley, panorama_rightAlley,panorama_courtyard, panorama_hall, panorama_roomB, panorama_roomA);
     } else if (toggle.value == 'night') {
         viewer.dispose();
         loadNight();
@@ -65,5 +99,5 @@ toggle.addEventListener('change', () => {
 
 (function init() {
     loadDay();
-    viewer.add(panorama_courtyard, panorama_hall, panorama_roomB, panorama_roomA);
+    viewer.add(panorama_foreyard, panorama_leftAlley, panorama_rightAlley, panorama_courtyard, panorama_hall, panorama_roomB, panorama_roomA);
 })();
